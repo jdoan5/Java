3391589662ac29ff98f91614c90e93180367122c
John Doan <30330279+jdoan5@users.noreply.github.com>
2026-09-07 17:07:13 -0400

    Java Interview

---
+++ Java Interview/src/Solution.java
@@ -0,0 +1,8 @@
+//Word Frequency Counter
+public class Solution {
+    public static void main(String[] args) {
+        String name = "John";
+        System.out.println("Hello " + name);
+    }
+}
+
---
+++ Java Interview/src/SolutionTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SolutionTest {
+
+    @Test
+    void testSomething() {
+        assertTrue(true,
+                "This is a sample test case");
+    }
+}
