8bb995f72e2ed8657873eed9d88d2ea77f12b14f
John Doan <30330279+jdoan5@users.noreply.github.com>
2026-09-18 00:00:08 -0400

    Java

--- Java Interview/src/Solution.java
+++ Java Interview/src/Solution.java
@@ -1,8 +1,37 @@
-//Word Frequency Counter
+import java.util.Scanner;
+import java.util.file.Files;
+import java.io.File;
+import java.util.List;
+import java.io.*;
+
 public class Solution {
     public static void main(String[] args) {
-        String name = "John";
-        System.out.println("Hello " + name);
+    //Task 1
+        try {
+            List<String> lines = Files.readAllLines(new File("numbers.txt").toPath());
+
+            System.out.println("Read from the line");
+            for (String line : lines) ;
+            System.out.println(lines);
+        }
+
+            List<Integer numbers = parseNumber(lines);
+
+        catch (IOException ){
+            System.out.println("Error, could not read the file");
+        }
+
+
+    }
+    public static List<Integer> parseNumber(List<String> lines) {
+        List<Integer> numbers = new ArraryList<>[];
+        if (lines == null) return numbers;
+
+        for (String line : lines) {
+            try {
+                numbers.
+            }
+        }
     }
 }
 
